8218c0d7d8d8c74c61d45b2349892b25ab227517
Jamiepod1 <jamieplumy@gmail.com>
2024-05-16 13:37:46 +0100

    added loop to create a grid of cells, store in nested list

--- cell.py
+++ cell.py
@@ -1,20 +1,28 @@
 import turtle as t
 
 class Cell(t.Turtle):
-    def __init__(self, rule=None):
+    def __init__(self):
         super().__init__()
         self.shape("square")
         self.color("white")
         self.state = 0
-        self.rule = rule
+        self.shapesize(stretch_wid=1, stretch_len=1, outline=0)
+        self.penup()
 
-    def state_change(self):
+    def __str__(self):
+        return f"{self.xcor()}, {self.ycor()}"
+    
+
+    def on(self):
+        if self.state == 0:
+            self.state = 1
+            self.color("black")
+
+
+    def off(self):
         if self.state == 1:
             self.state = 0
             self.color("white")
-        else:
-            self.state = 1
-            self.color("black")
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -2,18 +2,41 @@ import turtle as t
 import time
 from cell import Cell
 
+screen = t.Screen()
+screen.tracer(0)
+
+#set up grid of cells
+y_no_of_cells = 11
+x_no_of_cells = 11
+grid = []
+starting_x = -int(x_no_of_cells/2) * 20
+starting_y = int(y_no_of_cells/2) * 20
+
+
+for y in range(y_no_of_cells):
+    row = []
+    x_coord = starting_x
+    for x in range(x_no_of_cells):
+        y_coord = starting_y
+        cell = Cell()
+        cell.goto(x_coord, y_coord)
+        row.append(cell)
+        x_coord += 20
+    grid.append(row)
+    starting_y -= 20
+    
+
+screen.update()
+
+for row in grid:
+    for cell in row:
+        print(cell)
 
 cell = Cell()
-time.sleep(1)
-cell.state_change()
-time.sleep(1)
-cell.state_change()
-time.sleep(1)
-cell.state_change()
-time.sleep(1)
 cell.state_change()
 
 
+#set up rule
 
 
 
